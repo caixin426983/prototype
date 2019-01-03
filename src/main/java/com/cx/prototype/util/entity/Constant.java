bd3f02cb90140984745ffcfb722be82657065df9
caixin <836566274@qq.com>
2019-01-03 16:48:09 +0800

    增加了常量类，以后全局的常量都写这里面

---
+++ src/main/java/com/cx/prototype/util/entity/Constant.java
@@ -0,0 +1,26 @@
+package com.cx.prototype.util.entity;
+
+/**
+ * @Description 常量
+ * @Author cx
+ * @Date 2019/1/3 16:42
+ **/
+public class Constant {
+
+    //成功失败常量
+    public static final Integer SUCCESS_CODE = 200;
+    public static final Integer FAIL_CODE = 500;
+    public static final Integer NO_LOGIN_CODE = 401; // 未登录
+    public static final Integer NO_AUTH_CODE = 403; // 无权限
+
+    public static final String SUCCESS = "success";
+    public static final String FAILED = "failed";
+    public static final String SUCCESS_CN = "操作成功";
+    public static final String FAIL_CN = "操作失败";
+    public static final String NO_LOGIN_CN = "未登录";
+    public static final String NO_AUTH_CN = "无权限";
+    public static final String ILLEGAL_CN = "非法的用户";
+
+
+
+}
