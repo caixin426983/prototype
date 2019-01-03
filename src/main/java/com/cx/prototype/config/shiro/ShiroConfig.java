31aa64d6b2a8918c9843702992a2d14635e4ffed
caixin <836566274@qq.com>
2019-01-03 17:08:14 +0800

    增加了无权限返回和未登录返回

--- src/main/java/com/cx/prototype/config/shiro/ShiroConfig.java
+++ src/main/java/com/cx/prototype/config/shiro/ShiroConfig.java
@@ -44,7 +44,7 @@ public class ShiroConfig {
         filterChainDefinitionMap.put("/druid/**", "anon");
         filterChainDefinitionMap.put("/static/**", "anon");
         filterChainDefinitionMap.put("/ajaxLogin", "anon");
-        filterChainDefinitionMap.put("/login", "anon");
+
 
         //<!-- 过滤链定义，从上向下顺序执行，一般将/**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
         //<!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
@@ -52,7 +52,9 @@ public class ShiroConfig {
 
 
         // 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
-        shiroFilterFactoryBean.setLoginUrl("/unauth");
+        shiroFilterFactoryBean.setLoginUrl("/noLogin");
+        //设置无权限返回
+        shiroFilterFactoryBean.setUnauthorizedUrl("/unauth");
 
         // 登录成功后要跳转的链接
         //shiroFilterFactoryBean.setSuccessUrl("/index");
