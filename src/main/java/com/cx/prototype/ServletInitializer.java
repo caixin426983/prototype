7eeb9091d2439ea774c4a73940309e665a13112d
caixin <836566274@qq.com>
2019-01-07 16:52:51 +0800

    这个类是去掉内嵌的tomcat后来启动项目的类

---
+++ src/main/java/com/cx/prototype/ServletInitializer.java
@@ -0,0 +1,17 @@
+package com.cx.prototype;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @Description
+ * @Author cx
+ * @Date 2019/1/7 10:45
+ **/
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PrototypeApplication.class);
+    }
+}
